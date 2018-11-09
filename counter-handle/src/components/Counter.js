aa8e7c7fcf12eb77e08a14c4dd1d14306ded3311
Le Van Toan <toanlv@smartosc.com>
2018-11-09 13:29:11 +0700

    add counter app

---
+++ counter-handle/src/components/Counter.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  render() {
+    return (
+      <div className="Counter">
+        <h1>Hello world from counter</h1>
+      </div>
+    );
+  }
+}
+
+export default Counter;
---
+++ counter-handle/src/containers/Counters.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Counter from '../components/Counter';
+
+
+class Counters extends Component {
+  render() {
+    return (
+      <div className="Counters">
+        <Counter />
+      </div>
+    );
+  }
+}
+
+export default Counters;
